a973ccb7a68c9b29a9a2a08721de43c984afd332
KonLew <surangrat231@gmail.com>
2023-01-17 16:48:20 +0700

    lab 15_1 com

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+for(int i = 1; i < N; i++){
+        cout << "Pass " << i <<":";
+        for(int j = i; j > 0; j--){
+            if(d[j] > d[j-1]){
+                T temp = d[j];
+                d[j] = d[j-1];
+                d[j-1] = temp;
+            }
+        }
+    
+        for(int k = 0; k < N; k++){
+            cout << d[k] << " ";
+        }
+        cout << "\n";
+    }
 
 }
 
@@ -17,3 +32,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
